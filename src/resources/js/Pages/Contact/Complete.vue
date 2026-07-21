a0cbef3b65f6bad1559437b546cb879acd042a58
Ayumi Mitani <ayumimitani3@gmail.com>
2026-07-21 21:10:39 +0900

    送信完了画面の閉じるボタンを実装

--- src/resources/js/Pages/Contact/Complete.vue
+++ src/resources/js/Pages/Contact/Complete.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+// Inertiaの画面遷移機能を使用するためにrouterを読み込む
+import { router } from '@inertiajs/vue3';
+
+/**
+ * 「閉じる」ボタン押下時の処理
+ * お問い合わせ入力画面へ画面遷移する
+ */
+const backToForm = () => {
+    // 入力画面（/）へ画面遷移する
+    router.get('/');
+};
+</script>
+
 <template>
     <h1>お問い合わせフォーム（送信完了）</h1>
 
@@ -7,12 +21,9 @@
         折り返し弊社担当者よりご連絡させていただきます。
     </div>
 
-    <button type="button">閉じる</button>
+    <button type="button" @click="backToForm">閉じる</button>
 
 </template>
-
-<script setup lang="ts">
-</script>
 
 <style scoped>
 </style>
