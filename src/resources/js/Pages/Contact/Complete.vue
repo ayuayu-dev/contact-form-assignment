fe345a4864274965d9868a3a8cd17a9d3cc65f4b
Ayumi Mitani <ayumimitani3@gmail.com>
2026-07-22 13:25:54 +0900

    送信完了画面のデザインを調整

--- src/resources/js/Pages/Contact/Complete.vue
+++ src/resources/js/Pages/Contact/Complete.vue
@@ -13,17 +13,37 @@ const backToForm = () => {
 </script>
 
 <template>
-    <h1>お問い合わせフォーム</h1>
+    <h1 class="contact-form-title">お問い合わせフォーム</h1>
 
-    <div>
-        お問い合わせを受け付けました。<br>
-        お問い合わせ内容はご登録いただいたメールアドレス宛にお送りしております。<br>
-        折り返し弊社担当者よりご連絡させていただきます。
+    <div class="contact-form">
+
+        <div  class="complete-message">
+            <p>お問い合わせを受け付けました。</p>
+            <p>
+                お問い合わせ内容はご登録いただいたメールアドレス宛に
+                お送りしております。
+            </p>
+            <p>
+                折り返し弊社担当者よりご連絡させていただきます。
+            </p>
+        </div>
+
+        <div class="button-area">
+            <button class="cancel-button" type="button" @click="backToForm">閉じる</button>
+        </div>
+
     </div>
-
-    <button type="button" @click="backToForm">閉じる</button>
-
 </template>
 
 <style scoped>
+/* 送信完了メッセージの下に余白を設定する */
+.complete-message {
+    margin-bottom: 40px;
+}
+
+/* 閉じるボタンを中央に配置する */
+.button-area {
+    display: flex;
+    justify-content: center;
+}
 </style>
